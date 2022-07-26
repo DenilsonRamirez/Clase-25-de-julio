6a427eac4c0703564184dce1a6f3411a6100481d
Denilson <denixmas123@gmail.com>
2022-07-25 18:54:57 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE> html>
+<html lang="eng">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, inital-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+    <h1>Prueba de funcionamiento de github</h1>
+    <p><h2>Haciendo despliegue en HEROKU</h2></p>
+	<p>
+		<a href="funcionamiento.html">Ir a funcionamiento</a>
+    </p>
+</body>
+</html>
